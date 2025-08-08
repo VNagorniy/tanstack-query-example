0341161c12a2d65f43d6a5101012ea7e688ea292
VNagorniy <vnagorniyrnd@gmail.com>
2025-08-08 14:59:48 +0300

    feat: preloader, fetchStatus

--- src/features/playlists.tsx
+++ src/features/playlists.tsx
@@ -4,13 +4,23 @@ import { client } from '../shared/api/client';
 export const Playlists = () => {
 	const query = useQuery({
 		queryKey: ['playlists'],
-		queryFn: () => client.GET('/playlists')
+		queryFn: async () => {
+			const response = await client.GET('/playlistsXXX' as unknown as '/playlists');
+			if (response.error) {
+				throw (response as unknown as { error: Error }).error;
+			}
+			return response.data;
+		}
 	});
+
+	if (query.isPending) return <span>Loading...</span>;
+	if (query.isError) return <span>Error: {JSON.stringify(query.error)}</span>;
 
 	return (
 		<div>
+			{query.isFetching ? 'TIME' : ''}
 			<ul>
-				{query.data?.data?.data.map((playlist) => (
+				{query.data.data.map((playlist) => (
 					<li key={playlist.id}>{playlist.attributes.title}</li>
 				))}
 			</ul>
