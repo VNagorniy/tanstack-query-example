9aa166af10971f9787d66acae25ac7f119884d30
VNagorniy <vnagorniyrnd@gmail.com>
2025-08-11 22:32:01 +0300

    feat: pagination, placeholderData

--- src/features/playlists.tsx
+++ src/features/playlists.tsx
@@ -1,16 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { client } from '../shared/api/client';
+import { Pagination } from '../shared/ui/pagination/pagination';
+import { useState } from 'react';
 
 export const Playlists = () => {
+	const [page, setPage] = useState(1);
+
 	const query = useQuery({
-		queryKey: ['playlists'],
+		queryKey: ['playlists', page],
 		queryFn: async () => {
-			const response = await client.GET('/playlistsXXX' as unknown as '/playlists');
+			const response = await client.GET('/playlists', {
+				params: {
+					query: {
+						pageNumber: page
+					}
+				}
+			});
 			if (response.error) {
 				throw (response as unknown as { error: Error }).error;
 			}
 			return response.data;
-		}
+		},
+		placeholderData: keepPreviousData
 	});
 
 	if (query.isPending) return <span>Loading...</span>;
@@ -18,7 +29,7 @@ export const Playlists = () => {
 
 	return (
 		<div>
-			{query.isFetching ? 'TIME' : ''}
+			<Pagination pagesCount={query.data.meta.pagesCount} currentPage={query.data.meta.page} onPageNumberChange={setPage} isFetching={query.isFetching} />
 			<ul>
 				{query.data.data.map((playlist) => (
 					<li key={playlist.id}>{playlist.attributes.title}</li>
