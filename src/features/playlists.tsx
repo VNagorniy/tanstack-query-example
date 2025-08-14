c5879803ea2a11a70a9a6803fee80942aae9f9ef
VNagorniy <vnagorniyrnd@gmail.com>
2025-08-14 21:14:48 +0300

    feat: abortController, Object as queryKey

--- src/features/playlists.tsx
+++ src/features/playlists.tsx
@@ -5,16 +5,19 @@ import { useState } from 'react';
 
 export const Playlists = () => {
 	const [page, setPage] = useState(1);
+	const [search, setSearch] = useState('');
 
 	const query = useQuery({
-		queryKey: ['playlists', page],
-		queryFn: async () => {
+		queryKey: ['playlists', { page, search }],
+		queryFn: async ({ signal }) => {
 			const response = await client.GET('/playlists', {
 				params: {
 					query: {
-						pageNumber: page
+						pageNumber: page,
+						search
 					}
-				}
+				},
+				signal
 			});
 			if (response.error) {
 				throw (response as unknown as { error: Error }).error;
@@ -29,12 +32,18 @@ export const Playlists = () => {
 
 	return (
 		<div>
-			<Pagination pagesCount={query.data.meta.pagesCount} currentPage={query.data.meta.page} onPageNumberChange={setPage} isFetching={query.isFetching} />
-			<ul>
-				{query.data.data.map((playlist) => (
-					<li key={playlist.id}>{playlist.attributes.title}</li>
-				))}
-			</ul>
+			<div>
+				<input value={search} onChange={(e) => setSearch(e.currentTarget.value)} placeholder={'search...'} />
+			</div>
+			<hr />
+			<div>
+				<Pagination pagesCount={query.data.meta.pagesCount} currentPage={query.data.meta.page} onPageNumberChange={setPage} isFetching={query.isFetching} />
+				<ul>
+					{query.data.data.map((playlist) => (
+						<li key={playlist.id}>{playlist.attributes.title}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
